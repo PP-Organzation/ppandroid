445b3e9bbc458f078de0a04bfbebe1e3ba347b84
yeqinfu <990761790@qq.com>
2016-04-03 10:44:38 +0800

    !ac_main

--- app/src/main/java/com/ppandroid/whitefm/AC_Main.java
+++ app/src/main/java/com/ppandroid/whitefm/AC_Main.java
@@ -1,17 +1,26 @@
 package com.ppandroid.whitefm;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.antonioleiva.mvpexample.app.R;
 import com.ppandroid.whitefm.base.AC_Base;
+
+import butterknife.Bind;
 
 /**
  * Created by yeqinfu on 16-4-2.
  */
 public class AC_Main extends AC_Base{
+    @Bind(R.id.tv_show)
+    TextView tv_show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.ac_main);
+        tv_show.setText("afteroncreate");
+        Toast.makeText(AC_Main.this,"dasfa",Toast.LENGTH_LONG).show();
     }
+
 }
--- app/src/main/java/com/ppandroid/whitefm/base/AC_Base.java
+++ app/src/main/java/com/ppandroid/whitefm/base/AC_Base.java
@@ -1,8 +1,21 @@
 package com.ppandroid.whitefm.base;
 
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+
 /**
  * Created by yeqinfu on 16-4-2.
  */
 public class AC_Base extends AC_ToolBar{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        ButterKnife.bind(this);//绑定注解
+    }
 }
