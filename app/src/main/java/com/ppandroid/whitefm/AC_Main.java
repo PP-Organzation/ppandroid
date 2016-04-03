8d1493f70899cf7b8e82df7d85d0c7ac4966558e
yeqinfu <990761790@qq.com>
2016-04-03 08:36:36 +0800

    AC_Main

---
+++ app/src/main/java/com/ppandroid/whitefm/AC_Main.java
@@ -0,0 +1,17 @@
+package com.ppandroid.whitefm;
+
+import android.os.Bundle;
+
+import com.antonioleiva.mvpexample.app.R;
+import com.ppandroid.whitefm.base.AC_Base;
+
+/**
+ * Created by yeqinfu on 16-4-2.
+ */
+public class AC_Main extends AC_Base{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
